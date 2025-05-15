b5c4acf4bc4869bdb3c56b5123d8630cd2e9ee32
Kimgumi <79128508+Kimgumi@users.noreply.github.com>
2025-05-15 18:37:34 +0900

    section01
    promotion 부분 페이지 제작

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -25,6 +25,7 @@ window.addEventListener('scroll', _.throttle(function(){
   if (window.scrollY > 500) { // 배지 숨기기
     // gsap.to(요소,지속시간,옵션);
     // gsap는 애니메이션을 처리해주는 라이브러리
+    // gsap 라이브러리는 CSS만 가지고서 제어하기에 한계가 있던 걸 제어한 것 
     gsap.to(badgeEl, .6, {// 객체 데이터
       opacity: 0,
       display: 'none'
@@ -37,3 +38,45 @@ window.addEventListener('scroll', _.throttle(function(){
     });
   }
 },300));
+
+const fadeELs = document.querySelectorAll('.visual .fade-in');
+fadeELs.forEach(function(fadeEL, index){
+  gsap.to(fadeEL, 1, {
+    delay: (index + 1) * .7, // 0.7초 간격으로 애니메이션이 발생하며 opacity 1로 1초동안 변한다 
+    opacity: 1
+  });
+});
+
+// 슬라이드 요소 관리 - new Swiper(선택자, 옵션);
+new Swiper('.notice-line .mySwiper', {
+  direction: 'vertical',
+  autoplay: true, // 자동 재생
+  loop: true // 반복 재생
+});
+new Swiper('.promotion .mySwiper', {
+  // direction: 'horizontal'=> 기본값이라 안 써도 됌
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 1, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true, 
+  pagination: { // 페이지 번호 요소 선택자
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자가 페이지 번호 요소에 대한 제어 가능 여부
+  },
+  navigation: { // 다음 버튼 요소 선택자
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false; // 프로모션 영역 숨김 여부(flase: 보임, true: 숨김)
+promotionToggleBtn.addEventListener('click',function(){
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) { // 숨김 처리 
+    promotionEl.classList.add('hide'); 
+  } else { // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
